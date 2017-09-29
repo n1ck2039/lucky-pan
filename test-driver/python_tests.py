431ae54ea1ee3f938b6f2ef590304509871dc868
Nick <nickinns@gmail.com>
2017-09-28 18:39:31 -0700

    Update python_tests.py

--- test-driver/python_tests.py
+++ test-driver/python_tests.py
@@ -16,7 +16,7 @@ class TestMB(unittest.TestCase):
         r1 = requests.get(target_url + "/v1/pets/lucky")
         self.assertEqual(r1.status_code, 200)
         response_body = json.loads(r1.text)
-        self.assertEqual(response_body['name'], 'Lucky')
+        self.assertEqual(response_body['name'], 'Max')
         self.assertEqual(response_body['type'], 'Dog')
         self.assertEqual(response_body['breed'], 'mixed')
 
